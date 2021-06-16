a21e971aca4e8e1eec609db3ded6733d7b0cfa2f
Amilashan98 <amilashanes@gamil.com>
2021-06-16 12:36:29 +0530

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width-device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie-edge">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <title>Document</title>
+    </head>
+    <body>
+        <form>
+            <div class="form-group">
+                <label for="formGroupExampleInput">Name</label>
+                <div class="col-sm-5">
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Enter Name">
+              </div>
+
+              <div class="form-group">
+                <label for="formGroupExampleInput">Username</label>
+                <div class="col-sm-5">
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Enter User name">
+              </div>
+
+              <div class="form-group">
+                <label for="formGroupExampleInput">Date of Birth</label>
+                <div class="col-sm-5">
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Enter date of Birth">
+              </div>
+
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <div class="col-sm-5">
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+              <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+
+            <div class="form-group">
+                <label for="formGroupExampleInput">Telephone NO</label>
+                <div class="col-sm-5">
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Enter telephone">
+              </div>
+
+              <div class="form-group">
+                <label for="formGroupExampleInput">NIC NO.</label>
+                <div class="col-sm-5">
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Enter NIC">
+              </div>
+
+              <fieldset class="form-group row">
+                <legend class="col-form-legend col-sm-2">Gender</legend>
+                <div class="col-sm-10">
+                  <div class="form-check">
+                    <label class="form-check-label">
+                      <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
+                      Male
+                    </label>
+                  </div>
+                  <div class="form-check">
+                    <label class="form-check-label">
+                      <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
+                      Female
+                    </label>
+                  </div>
+                  
+                </div>
+              </fieldset>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <div class="col-sm-5">
+              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>
+            
+            <div class="form-group">
+                <label for="exampleInputPassword1">Re enter password</label>
+                <div class="col-sm-5">
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="RE Enter Password">
+              </div>
+              <button type="submit" class="btn btn-primary my-1">Submit</button>
+          </form>
+    </body>
+</html>
